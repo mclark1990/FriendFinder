77134db40aaea4978df3f5d139b945fbb9637b14
mclark1990 <mjc8875@gmail.com>
2018-08-18 21:28:28 -0400

    added package.json

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ var express = require("express");
 
 var app = express();
 
-var PORT = process.env.PORT || 8080;
+var PORT = process.env.PORT || 3000;
 
 app.use(bodyParser.urlencoded({ extended: true}));
 app.use(bodyParser.json());
